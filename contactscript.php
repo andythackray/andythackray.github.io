eed7914cb522a96150e79dd8271f083f8147a40d
33702528 <thackrayaf@gmail.com>
2020-04-17 15:48:12 +0100

    Updated PHP

--- contactscript.php
+++ contactscript.php
@@ -1,56 +1,56 @@
-<?php
-	// VALUES FROM THE FORM
+php
+	 VALUES FROM THE FORM
 	$name		= $_POST['name'];
 	$email		= $_POST['email'];
-	$message	= $_POST['msg'];
+	$message	= $_POST['message'];
 
-	// ERROR & SECURITY CHECKS
-	if ( ( !$email ) ||
-		 ( strlen($_POST['email']) > 200 ) ||
-	     ( !preg_match("#^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$#", $email) )
+	 ERROR & SECURITY CHECKS
+	if ( ( !$email ) 
+		 ( strlen($_POST['email'])  200 ) 
+	     ( !preg_match(#^[A-Za-z0-9](([_.-][a-zA-Z0-9]+))@([A-Za-z0-9]+)(([.-][a-zA-Z0-9]+)).([A-Za-z]{2,})$#, $email) )
        ) 
 	{ 
-		print "Error: Invalid E-Mail Address"; 
+		print Error Invalid E-Mail Address; 
 		exit; 
 	} 
-	if ( ( !$name ) ||
-		 ( strlen($name) > 100 ) ||
-		 ( preg_match("/[:=@\<\>]/", $name) ) 
+	if ( ( !$name ) 
+		 ( strlen($name)  100 ) 
+		 ( preg_match([=@], $name) ) 
 	   )
 	{ 
-		print "Error: Invalid Name"; 
+		print Error Invalid Name; 
 		exit; 
 	} 
-	if ( preg_match("#cc:#i", $message, $matches) )
+	if ( preg_match(#cc#i, $message, $matches) )
 	{ 
-		print "Error: Found Invalid Header Field"; 
+		print Error Found Invalid Header Field; 
 		exit; 
 	} 
 	if ( !$message )
 	{
-		print "Error: No Message"; 
+		print Error No Message; 
 		exit; 
 	} 
-	if (eregi("\r",$email) || eregi("\n",$email)){ 
-		print "Error: Invalid E-Mail Address"; 
+	if (eregi(r,$email)  eregi(n,$email)){ 
+		print Error Invalid E-Mail Address; 
 		exit; 
 	} 
 	if (FALSE) { 
-		print "Error: You cannot send to an email address on the same domain."; 
+		print Error You cannot send to an email address on the same domain.; 
 		exit; 
 	} 
 
 
-	// CREATE THE EMAIL
-	$headers	= "Content-Type: text/plain; charset=iso-8859-1\n";
-	$headers	.= "From: $name <$email>\n";
-	$recipient	= "thackrayaf@gmail.com";
-	$subject	= "New message from your website";
+	 CREATE THE EMAIL
+	$headers	= Content-Type textplain; charset=iso-8859-1n;
+	$headers	.= From $name $emailn;
+	$recipient	= thackrayaf@gmail.com;
+	$subject	= New message from your website;
 	$message	= wordwrap($message, 1024);
 
-	// SEND THE EMAIL TO YOU
+	 SEND THE EMAIL TO YOU
 	mail($recipient, $subject, $message, $headers);
 
-	// REDIRECT TO THE THANKS PAGE
-	header("location: thanks.php");
-?>
+	 REDIRECT TO THE THANKS PAGE
+	header(location thanks.php);
+
